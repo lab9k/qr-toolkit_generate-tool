77f66cd903ba2fd90b5bf732fce2cb0ac0c5fe4e
Jefwillems <jefwillems100@gmail.com>
2020-04-15 11:41:01 +0200

    setup project

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import SingleView from '../views/SingleView.vue';
+import MultiView from '../views/MultiView.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/single',
+    name: 'Single',
+    component: SingleView,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/multi',
+    name: 'Multi',
+    component: MultiView,
+  },
+  {
+    path: '/',
+    redirect: '/single',
   },
 ];
 
---
+++ src/views/SingleView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Single</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SingleModeForm',
+  data() {
+    return {};
+  },
+};
+</script>
